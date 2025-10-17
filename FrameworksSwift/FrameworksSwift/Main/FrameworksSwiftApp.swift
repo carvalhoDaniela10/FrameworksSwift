0fb3b15a5824a1ace13f1c60483bc922f582d86b
Daniela Valadares <danielasvc.ti@gmail.com>
2025-10-17 15:32:28 -0300

    Adicione projeto ao repositório

---
+++ FrameworksSwift/FrameworksSwift/Main/FrameworksSwiftApp.swift
@@ -0,0 +1,32 @@
+//
+//  FrameworksSwiftApp.swift
+//  FrameworksSwift
+//
+//  Created by Daniela Valadares on 17/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FrameworksSwiftApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
